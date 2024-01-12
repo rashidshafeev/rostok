a7ce4afde7a4492bcd2ed6a217f347aea860cdd6
beks <turgunbaaev@gmail.com>
2024-01-12 21:00:10 +0300

    fix grid elements

--- src/components/Home/FurnitureFittings.jsx
+++ src/components/Home/FurnitureFittings.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { products } from '../../constants/data';
 import ProductCard from '../ProductCard';
 
@@ -7,24 +8,28 @@ const FurnitureFittings = () => {
       <h1 className='text-colBlack text-4xl font-semibold pb-4'>
         Мебельная фурнитура
       </h1>
-      <div className='flex space-x-5'>
-        <div className='flex-grow'>
-          <div className='grid grid-cols-4 gap-5'>
-            {products?.slice(0, 12)?.map((el) => (
-              <ProductCard key={el?.id} product={el} furniture={true} />
-            ))}
-          </div>
-        </div>
-        <div className='flex-grow-0 flex-shrink-0 h-full w-1/3 bg-[#CFE0D3] px-5 py-3 rounded-xl'>
-          <h3 className='text-lg font-semibold text-colBlack pb-4'>
-            Рекомендуем для вас
-          </h3>
-          <div className='grid grid-cols-2 gap-5'>
-            {products?.slice(0, 4)?.map((el) => (
-              <ProductCard key={el?.id} product={el} recommended={true} />
-            ))}
-          </div>
-        </div>
+      <div className='grid grid-cols-6 gap-5'>
+        {products?.map((el, index) => (
+          <React.Fragment key={el?.id}>
+            <ProductCard product={el} furniture={true} />
+            {index === 3 && (
+              <div className='col-span-2 col-start-5 col-end-7 row-start-1 row-end-3 bg-[#CFE0D3] px-5 py-3 rounded-xl'>
+                <h3 className='text-lg font-semibold text-colBlack pb-4'>
+                  Рекомендуем для вас
+                </h3>
+                <div className='grid grid-cols-2 gap-5'>
+                  {products?.slice(0, 4)?.map((recommendedEl) => (
+                    <ProductCard
+                      key={recommendedEl?.id}
+                      product={recommendedEl}
+                      recommended={true}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
