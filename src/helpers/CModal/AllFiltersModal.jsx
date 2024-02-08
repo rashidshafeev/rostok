d5bc238921fdab9362f20dac947c197b170fe96b
beks <turgunbaaev@gmail.com>
2024-02-08 13:19:33 +0300

    All Filters

--- src/helpers/CModal/AllFiltersModal.jsx
+++ src/helpers/CModal/AllFiltersModal.jsx
@@ -1,6 +1,15 @@
 import { Box, Modal } from '@mui/material';
+import { allFilters } from '../../constants/data';
+import { useState } from 'react';
+import { ArrowIcon } from '../Icons';
 
 const AllFiltersModal = ({ open, setOpen }) => {
+  const [accordion, setAccordion] = useState(null);
+
+  const toggleAccordion = (id) => {
+    setAccordion(accordion === id ? null : id);
+  };
+
   if (!open) return null;
 
   return (
@@ -10,7 +19,7 @@ const AllFiltersModal = ({ open, setOpen }) => {
       aria-labelledby='modal-modal-title'
       aria-describedby='modal-modal-description'
     >
-      <Box className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] h-[90%] lining-nums proportional-nums bg-white outline-none rounded-lg border-none p-6 overflow-hidden'>
+      <Box className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[85%] h-[90%] lining-nums proportional-nums bg-white outline-none rounded-lg border-none p-6 overflow-hidden'>
         <div className='flex flex-col justify-between h-full'>
           <div className='h-[90%]'>
             <div className='flex justify-between items-center'>
@@ -25,7 +34,28 @@ const AllFiltersModal = ({ open, setOpen }) => {
               </span>
             </div>
             <div className='mt-2 border-t border-b border-[#EBEBEB] overflow-y-scroll overflow-hidden h-[93%]'>
-              <div className='h-[1200px]'></div>
+              <div className='h-[1200px] pt-5'>
+                <div className='grid grid-cols-3 gap-5'>
+                  {allFilters?.map((el) => (
+                    <div
+                      key={el?.id}
+                      className='flex justify-between items-center cursor-pointer'
+                      onClick={() => toggleAccordion(el?.id)}
+                    >
+                      <span className='text-colBlack font-semibold'>
+                        {el?.name}
+                      </span>
+                      <ArrowIcon
+                        className={`!m-0 !w-4 !h-4 ${
+                          accordion === el?.id
+                            ? 'rotate-[0deg]'
+                            : 'rotate-[180deg]'
+                        }`}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
           <div className='flex space-x-3 h-10'>
