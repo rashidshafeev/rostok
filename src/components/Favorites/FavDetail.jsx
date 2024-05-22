dab6134bb546fe3bf5fffee3d17e8a1514f97f1d
beks <turgunbaaev@gmail.com>
2024-05-22 10:44:21 +0300

    Favorites cards

--- src/components/Favorites/FavDetail.jsx
+++ src/components/Favorites/FavDetail.jsx
@@ -1,10 +1,9 @@
-import { Fragment } from 'react';
 import ProductCard from '../ProductCard';
 import filterIcon from '../../assets/icons/filter.svg';
 
 const FavDetail = ({ favorite, user }) => {
   return (
-    <div className={`${user && 'md:pl-5'} pb-10 min-h-[560px]`}>
+    <div className={`${user && 'md:pl-5'} pb-10 min-h-[560px] w-full`}>
       <button className='flex md:hidden items-center ml-auto outline-none bg-transparent mt-4 md:mt-0'>
         <img src={filterIcon} alt='*' />
         <span className='text-colBlack text-xs font-medium pt-[2px]'>
@@ -13,13 +12,13 @@ const FavDetail = ({ favorite, user }) => {
       </button>
       <div
         className={`${
-          user && 'md:grid-cols-2'
-        } grid grid-cols-2 mm:grid-cols-3 lg:grid-cols-3 ll:grid-cols-4 gap-3 xl:grid-cols-5 pt-4`}
+          user
+            ? 'grid-cols-2 mm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 ll:grid-cols-4 xl:grid-cols-5'
+            : 'grid-cols-2 mm:grid-cols-3 lg:grid-cols-4 ll:grid-cols-5 xl:grid-cols-6'
+        } grid gap-3 pt-4`}
       >
         {favorite?.map((el) => (
-          <Fragment key={el?.id}>
-            <ProductCard product={el} />
-          </Fragment>
+          <ProductCard key={el?.id} product={el} />
         ))}
       </div>
     </div>
