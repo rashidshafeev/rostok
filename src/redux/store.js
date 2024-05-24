2432c9f83b729a220e6555e1323da87ff2742685
beks <turgunbaaev@gmail.com>
2024-05-24 15:22:11 +0300

    Clear code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,12 @@
-import { combineReducers, configureStore, createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
-import cartReducer, { addToCart, removeFromCart } from './slices/cartSlice';
+import cartReducer from './slices/cartSlice';
 import productsReducer from './slices/productsSlice';
-import { api  } from './api/api';
-import favoriteReducer, { toggleFavorite } from './slices/favoriteSlice';
-import comparisonReducer, { toggleComparison } from './slices/comparisonSlice';
-import organizationsReducer, { addOrganization } from './slices/organizationsSlice';
+import { api } from './api/api';
+import favoriteReducer from './slices/favoriteSlice';
+import comparisonReducer from './slices/comparisonSlice';
+import organizationsReducer from './slices/organizationsSlice';
 import { listenerMiddleware } from './listeners';
-
-
-
-const persistConfig = {
-  key: 'root',
-  version: 1,
-  storage,
-};
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -28,19 +18,14 @@ const rootReducer = combineReducers({
   [api.reducerPath]: api.reducer,
 });
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  // reducer: persistedReducer,
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     })
-    .prepend(listenerMiddleware.middleware)
-    .concat(api.middleware),
+      .prepend(listenerMiddleware.middleware)
+      .concat(api.middleware),
 });
 
 export default store;
-
-// export const persistor = persistStore(store);
