0c374012e015768c356cbdabbc3b41037074b452
beks <turgunbaaev@gmail.com>
2024-07-05 14:52:27 +0300

    Reset password

--- src/redux/api/userEndpoints.js
+++ src/redux/api/userEndpoints.js
@@ -2,35 +2,40 @@
 import { api } from './api';
 
 export const userEndpoints = (builder) => ({
-    authWithEmail: builder.mutation({
-        query: (data) => 
-        ({
-            url: '/api/User/auth',
-            method: 'POST',
-            body: data,
-        })
+  authWithEmail: builder.mutation({
+    query: (data) => ({
+      url: '/api/User/auth',
+      method: 'POST',
+      body: data,
     }),
-    getUserData: builder.query({
-        query: ()  => 
-         ({
-            url: '/api/UserData/get',
-            method: 'GET',
-            prepareHeaders: (headers, { getState }) => {
-                // Access the token from the Redux store
-                const token = getState().user.token;
-                if (token) {
-                  headers.set('Authorization', `Bearer ${token}`);
-                }
-                return headers;
-              },
-         }),
-         keepUnusedDataFor: 0
+  }),
+  resetPassword: builder.mutation({
+    query: (email) => ({
+      url: '/api/User/sendPasswordResetLink',
+      method: 'POST',
+      body: { email: email },
     }),
-    
+  }),
+  getUserData: builder.query({
+    query: () => ({
+      url: '/api/UserData/get',
+      method: 'GET',
+      prepareHeaders: (headers, { getState }) => {
+        // Access the token from the Redux store
+        const token = getState().user.token;
+        if (token) {
+          headers.set('Authorization', `Bearer ${token}`);
+        }
+        return headers;
+      },
+    }),
+    keepUnusedDataFor: 0,
+  }),
 });
 
 // Export hooks for favorites endpoints
 export const {
-    useAuthWithEmailMutation,
-    useGetUserDataQuery
+  useAuthWithEmailMutation,
+  useResetPasswordMutation,
+  useGetUserDataQuery,
 } = api.injectEndpoints({ endpoints: userEndpoints });
