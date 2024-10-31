5fe9a71b5d3cf930850256dc59c946684caedc00
Rashid Shafeev <rashidshafeev@gmail.com>
2024-10-31 13:14:58 +0300

    Config fixes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import tsconfigPaths from 'vite-tsconfig-paths';
-import { fileURLToPath } from 'url';
-import path from "path";
+import { fileURLToPath, URL } from 'url';
 
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
@@ -13,16 +9,11 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url)),
-      // '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      // '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
-      // '@store': fileURLToPath(new URL('./src/redux', import.meta.url)),
-      // '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
-'@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@store': path.resolve(__dirname, 'src/redux'),
-      '@types': path.resolve(__dirname, 'src/types'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@store': fileURLToPath(new URL('./src/redux', import.meta.url)),
+      '@customTypes': fileURLToPath(new URL('./src/types', import.meta.url)),
     },
   },
   server: {
